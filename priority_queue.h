cfb5e95190ced066db7a895cb1b9c3af836e7c2f
nguyenmoney <117029682+nguyenmoney@users.noreply.github.com>
2025-06-09 13:36:14 +0700

    Update priority_queue.h

--- priority_queue.h
+++ priority_queue.h
@@ -11,7 +11,7 @@ typedef struct {
     int dungLuong;
 } HangDoiUuTienMotKhoa;
 
-// --- Khai báo các hàm ---
+// --- Khai bao cac ham  ---
 
 void khoiTaoHangDoiMotKhoa(HangDoiUuTienMotKhoa* hqmk);
 /*
